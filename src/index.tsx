b8984e45766c5db76e6f8500b8b9eee602598b55
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-23 23:09:27 +0700

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
 
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import thunkMiddleware from "redux-thunk"
 
-import { fetchSettings } from "./modules/settings/actions";
-import settings from "./lib/settings";
-import * as auth from "./lib/auth";
-import { connectToWebSocket } from "./lib/apiWebSocket";
-import reducers from "./rootReducer";
+import { fetchSettings } from "./modules/settings/actions"
+import settings from "./lib/settings"
+import * as auth from "./lib/auth"
+import { connectToWebSocket } from "./lib/apiWebSocket"
+import reducers from "./rootReducer"
 
-import "../public/css/flexboxgrid.min.css";
-import "../public/css/style.css";
+import "../public/css/flexboxgrid.min.css"
+import "../public/css/style.css"
 
-const DEVELOPER_MODE = settings.developerMode === true;
+const DEVELOPER_MODE = settings.developerMode === true
 if (DEVELOPER_MODE === false) {
-  auth.validateCurrentToken();
+  auth.validateCurrentToken()
 }
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
-store.dispatch(fetchSettings());
+const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+store.dispatch(fetchSettings())
 
 if (window.WebSocket) {
-  connectToWebSocket(store);
+  connectToWebSocket(store)
 } else {
-  console.log("WebSocket is not supported by your browser.");
+  console.log("WebSocket is not supported by your browser.")
 }
 
 ReactDOM.render(
@@ -38,9 +38,9 @@ ReactDOM.render(
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
