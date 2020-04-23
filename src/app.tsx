8bf4d04f8ee9186a5f9115501f641a86f0fdc2b7
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-23 22:00:42 +0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -3,40 +3,8 @@ import EditorJS from "@editorjs/editorjs"
 
 import Routes from "./routes/index"
 
-import {
-  blue700,
-  cyan700,
-  pinkA200,
-  grey100,
-  grey300,
-  grey400,
-  white,
-  darkBlack,
-  fullBlack,
-} from "material-ui/styles/colors"
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
-import getMuiTheme from "material-ui/styles/getMuiTheme"
-
 import "./App.css"
 
-const muiTheme = getMuiTheme({
-  fontFamily: "Roboto, sans-serif",
-  palette: {
-    primary1Color: blue700,
-    primary2Color: cyan700,
-    primary3Color: grey400,
-    accent1Color: pinkA200,
-    accent2Color: grey100,
-    accent3Color: blue700,
-    textColor: darkBlack,
-    alternateTextColor: white,
-    canvasColor: white,
-    borderColor: grey300,
-    pickerHeaderColor: blue700,
-    shadowColor: fullBlack,
-  },
-  appBar: {},
-})
 const editor = new EditorJS()
 function App() {
   return (
