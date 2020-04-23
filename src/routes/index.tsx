b609759aaa0aabaec52f6a527f4aa0824f718ea2
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-23 22:00:39 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,64 @@
 import React from "react"
-import { Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-import Head from "./modules/head"
-import Login from "./routes/login"
-import Logout from "./routes/logout"
-import Home from "./routes/home"
-import NotFound from "./routes/notFound"
-import Products from "./routes/products"
-import ProductImport from "./routes/products/import"
-import ProductDetails from "./routes/products/edit"
-import ProductCategories from "./routes/products/categories"
-import Customers from "./routes/customers"
-import CustomerDetails from "./routes/customers/edit"
-import CustomerGroups from "./routes/customers/groups"
-import Orders from "./routes/orders"
-import OrderDetails from "./routes/orders/edit"
-import OrderStatuses from "./routes/orders/statuses"
-import Pages from "./routes/pages"
-import PagesDetails from "./routes/pages/edit"
-import Settings from "./routes/settings"
-import Apps from "./routes/apps"
-import Files from "./routes/files"
+import Head from "./../modules/head"
+
+import Login from "./login"
+import Logout from "./logout"
+import Home from "./home"
+import NotFound from "./notFound"
+import Products from "./products"
+import ProductImport from "./products/import"
+import ProductDetails from "./products/edit"
+import ProductCategories from "./products/categories"
+import Customers from "./customers"
+import CustomerDetails from "./customers/edit"
+import CustomerGroups from "./customers/groups"
+import Orders from "./orders"
+import OrderDetails from "./orders/edit"
+import OrderStatuses from "./orders/statuses"
+import Pages from "./pages"
+import PagesDetails from "./pages/edit"
+import Settings from "./settings"
+import Apps from "./apps"
+import Files from "./files"
+
+import {
+  blue700,
+  cyan700,
+  pinkA200,
+  grey100,
+  grey300,
+  grey400,
+  white,
+  darkBlack,
+  fullBlack,
+} from "material-ui/styles/colors"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import getMuiTheme from "material-ui/styles/getMuiTheme"
+
+const muiTheme = getMuiTheme({
+  fontFamily: "Roboto, sans-serif",
+  palette: {
+    primary1Color: blue700,
+    primary2Color: cyan700,
+    primary3Color: grey400,
+    accent1Color: pinkA200,
+    accent2Color: grey100,
+    accent3Color: blue700,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    pickerHeaderColor: blue700,
+    shadowColor: fullBlack,
+  },
+  appBar: {},
+})
 
 function Routes() {
   return (
-    <Router>
+    <BrowserRouter>
       <MuiThemeProvider muiTheme={muiTheme}>
         <div id="container">
           <div id="headContainer">
@@ -68,7 +102,7 @@ function Routes() {
           </div>
         </div>
       </MuiThemeProvider>
-    </Router>
+    </BrowserRouter>
   )
 }
 
