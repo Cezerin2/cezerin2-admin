da58046cc8350a4366123314782ddb4e6b3d2db3
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-27 00:58:33 +0700

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,6 @@
+import { green, purple } from "@material-ui/core/colors"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
 import { Router } from "@reach/router"
-import {
-  blue700,
-  cyan700,
-  darkBlack,
-  fullBlack,
-  grey100,
-  grey300,
-  grey400,
-  pinkA200,
-  white,
-} from "material-ui/styles/colors"
-import getMuiTheme from "material-ui/styles/getMuiTheme"
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 import React from "react"
 import "./App.sass"
 import Head from "./modules/head"
@@ -35,59 +24,68 @@ import ProductDetails from "./routes/products/edit"
 import ProductImport from "./routes/products/import"
 import Settings from "./routes/settings"
 
-const muiTheme = getMuiTheme({
-  fontFamily: "Roboto, sans-serif",
+// const muiTheme = getMuiTheme({
+//   fontFamily: "Roboto, sans-serif",
+//   palette: {
+//     primary1Color: blue[700],
+//     primary2Color: cyan[700],
+//     primary3Color: grey[400],
+//     accent1Color: pink[200],
+//     accent2Color: grey100,
+//     accent3Color: blue[700],
+//     textColor: darkBlack,
+//     alternateTextColor: white,
+//     canvasColor: white,
+//     borderColor: grey[300],
+//     pickerHeaderColor: blue[700],
+//     shadowColor: fullBlack,
+//   },
+//   appBar: {},
+// })
+
+const theme = createMuiTheme({
   palette: {
-    primary1Color: blue700,
-    primary2Color: cyan700,
-    primary3Color: grey400,
-    accent1Color: pinkA200,
-    accent2Color: grey100,
-    accent3Color: blue700,
-    textColor: darkBlack,
-    alternateTextColor: white,
-    canvasColor: white,
-    borderColor: grey300,
-    pickerHeaderColor: blue700,
-    shadowColor: fullBlack,
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
   },
-  appBar: {},
 })
 
-function App() {
-  return (
-    <Router>
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div id="container">
-          <div id="headContainer">
-            <Head />
-          </div>
-          <div id="bodyContainer">
-            <Home path="/" />
-            <Login path="/login" />
-            <Logout path="/logout" />
-            <Products path="/products" />
-            <ProductImport path="/products/import" />
-            <ProductCategories path="/products/categories" />
-            <Orders path="/orders" />
-            <OrderStatuses path="/orders/statuses" />
-            <OrderDetails path="/order/:orderId" />
-            <Customers path="/customers" />
-            <CustomerGroups path="/customers/groups" />
-            <CustomerDetails path="/customer/:customerId" />
-            <ProductDetails path="/product/:productId" />
-            <Pages path="/pages" />
-            <PagesDetails path="/pages/add" />
-            <PagesDetails path="/pages/:pageId" />
-            <Settings path="/settings" />
-            <Apps path="/apps" />
-            <Files path="/files" />
-            <NotFound />
-          </div>
+const App = () => (
+  <Router>
+    <ThemeProvider theme={theme}>
+      <div id="container">
+        <div id="headContainer">
+          <Head />
         </div>
-      </MuiThemeProvider>
-    </Router>
-  )
-}
+        <div id="bodyContainer">
+          <Home path="/" />
+          <Login path="/login" />
+          <Logout path="/logout" />
+          <Products path="/products" />
+          <ProductImport path="/products/import" />
+          <ProductCategories path="/products/categories" />
+          <Orders path="/orders" />
+          <OrderStatuses path="/orders/statuses" />
+          <OrderDetails path="/order/:orderId" />
+          <Customers path="/customers" />
+          <CustomerGroups path="/customers/groups" />
+          <CustomerDetails path="/customer/:customerId" />
+          <ProductDetails path="/product/:productId" />
+          <Pages path="/pages" />
+          <PagesDetails path="/pages/add" />
+          <PagesDetails path="/pages/:pageId" />
+          <Settings path="/settings" />
+          <Apps path="/apps" />
+          <Files path="/files" />
+          <NotFound />
+        </div>
+      </div>
+    </ThemeProvider>
+  </Router>
+)
 
 export default App
