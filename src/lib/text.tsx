fa515f9135fff77acda0d4381f2c704c621fb1f3
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-27 01:19:23 +0700

    Adding languages

--- src/lib/text.tsx
+++ src/lib/text.tsx
@@ -1,3 +1,51 @@
-// import APPLICATION_TEXT from "./"
-const APPLICATION_TEXT = ""
+import Config from "../config"
+import Languages from "./locales"
+
+const { de, en, en_US, fr, it, pt_BR, ru, si, ta, uk, zh_CN } = Languages
+
+const lang = Config.language
+let selected = en_US
+
+switch (lang) {
+  case "de":
+    selected = de
+    break
+  case "en":
+    selected = en
+    break
+  case "en":
+    selected = en_US
+    break
+  case "en":
+    selected = fr
+    break
+  case "en":
+    selected = it
+    break
+  case "en":
+    selected = pt_BR
+    break
+  case "en":
+    selected = ru
+    break
+  case "en":
+    selected = si
+    break
+  case "en":
+    selected = ta
+    break
+  case "en":
+    selected = uk
+    break
+  case "en":
+    selected = zh_CN
+    break
+
+  default:
+    console.warn("Invalid language selected!")
+    break
+}
+
+const APPLICATION_TEXT = selected
+
 export default APPLICATION_TEXT
